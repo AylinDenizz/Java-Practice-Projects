f27dabd753866c29d4d78309e2307f2307018823
Aylin Deniz <119845693+AylinDenizz@users.noreply.github.com>
2023-07-21 14:28:38 +0300

    Update Main.java

--- star-triangle/src/Main.java
+++ star-triangle/src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+          public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number of steps: ");
+        int steps = input.nextInt();
+
+        drawInvertedTriangle(steps);
+    }
+
+    public static void drawInvertedTriangle(int steps) {
+        for (int i = steps; i >= 1; i--) {
+            // Print leading spaces
+            for (int j = 0; j < steps - i; j++) {
+                System.out.print(" ");
+            }
+            
+            // Print asterisks
+            for (int k = 0; k < 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println(); // Move to the next line after each row
+        }
     }
 }
+
